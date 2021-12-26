f199776b681cda6455c884d9ea3dedf6cf6cb221
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-12-26 14:40:51 +0530

    feat(init): add headers

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,19 @@
-import { Link, LiveReload, Outlet } from 'remix'
+import { Link, Links, LiveReload, Meta, Outlet } from 'remix'
 import globalStylesUrl from '~/styles/global.css'
 
+// Add Links and Meta to the global scope
+export const links = () => [{ rel: 'stylesheet', href: globalStylesUrl }]
+export const meta = () => {
+  const description = 'A simple remix blog'
+  const keywords = 'remix, blog, typescript, react'
+  const viewport = 'width=device-width, initial-scale=1'
+  const charset = 'utf-8'
+  return {
+    description, keywords, viewport, charset
+  }
+}
+
+// App root component
 const App: React.FC = () => {
   return (
     <Document title='Remix Blog'>
@@ -11,12 +24,14 @@ const App: React.FC = () => {
   )
 }
 
+// App document component
 const Document: React.FC<{ title: string }> = ({ children, title }) => {
   return (
     <html lang='en'>
       <head>
         <title>{title}</title>
-        <link rel='stylesheet' href={globalStylesUrl} />
+        <Links />
+        <Meta />
       </head>
       <body>
         {children}
@@ -26,6 +41,7 @@ const Document: React.FC<{ title: string }> = ({ children, title }) => {
   )
 }
 
+// App Layout component
 const Layout: React.FC = ({ children }) => {
   return (
     <>
